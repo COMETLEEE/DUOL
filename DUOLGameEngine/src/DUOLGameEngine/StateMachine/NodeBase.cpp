ec20cc6ffba6b5bcd0bd79dc470255c916bf1228
CrustPizza <53199927+CrustPizza@users.noreply.github.com>
2022-10-07 17:09:11 +0900

    NodeBase Getter 변경 & Event 구조 추가

--- DUOLGameEngine/src/DUOLGameEngine/StateMachine/NodeBase.cpp
+++ DUOLGameEngine/src/DUOLGameEngine/StateMachine/NodeBase.cpp
@@ -18,29 +18,14 @@ namespace DUOLGameEngine
 
 	}
 
-	const unsigned int NodeBase::GetUID() const
-	{
-		return _UID;
-	}
-
-	const std::string& NodeBase::GetName() const
-	{
-		return _name;
-	}
-
-	NodeType NodeBase::GetType() const
-	{
-		return _type;
-	}
-
-	NodeState NodeBase::GetState() const
-	{
-		return _state;
-	}
-
 	void NodeBase::SetState(NodeState state)
 	{
 		_state = state;
+	}
+
+	bool NodeBase::IsHalted() const
+	{
+		return _state == NodeState::IDLE;
 	}
 
 	bool NodeBase::IsRunning() const
@@ -51,5 +36,83 @@ namespace DUOLGameEngine
 	bool NodeBase::IsComplete() const
 	{
 		return (_state == NodeState::SUCCESS) || (_state == NodeState::FAILURE);
+	}
+
+	unsigned int NodeBase::AddPreEvent(PreEvent::EventType preEvent)
+	{
+		static unsigned int preEventID = 1;
+
+		auto result = _preEventManager.AddEvent(preEvent);
+
+		_preEventList.push_back({ preEventID, result });
+
+		return preEventID++;
+	}
+
+	bool NodeBase::SubPreEvent(unsigned int preEventID)
+	{
+		for (int i = 0; i < _preEventList.size(); i++)
+		{
+			if (_preEventList[i]._eventID == preEventID)
+			{
+				_preEventList.erase(_preEventList.begin() + i);
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	unsigned int NodeBase::AddPostEvent(PostEvent::EventType postEvent)
+	{
+		static unsigned int postEventID = 1;
+
+		auto result = _postEventManager.AddEvent(postEvent);
+
+		_postEventList.push_back({ postEventID, result });
+
+		return postEventID++;
+	}
+
+	bool NodeBase::SubPostEvent(unsigned int postEventID)
+	{
+		for (int i = 0; i < _postEventList.size(); i++)
+		{
+			if (_postEventList[i]._eventID == postEventID)
+			{
+				_postEventList.erase(_postEventList.begin() + i);
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	unsigned int NodeBase::AddChangeEvent(ChangeEvent::EventType changeEvent)
+	{
+		static unsigned int changeEventID = 1;
+
+		auto result = _changeEventManager.AddEvent(changeEvent);
+
+		_changeEventList.push_back({ changeEventID, result });
+
+		return changeEventID++;
+	}
+
+	bool NodeBase::SubChangeEvent(unsigned int changeEventID)
+	{
+		for (int i = 0; i < _changeEventList.size(); i++)
+		{
+			if (_changeEventList[i]._eventID == changeEventID)
+			{
+				_changeEventList.erase(_changeEventList.begin() + i);
+
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
--- DUOLGameEngine/src/DUOLGameEngine/StateMachine/NodeBase.h
+++ DUOLGameEngine/src/DUOLGameEngine/StateMachine/NodeBase.h
@@ -1,12 +1,28 @@
 #pragma once
-#include "DUOLGameEngine/StateMachine/NodeInfo.h"
-
 #include <string>
+#include <vector>
+#include <memory>
+
+#include "DUOLGameEngine/StateMachine/NodeInfo.h"
+#include "DUOLGameEngine/Event/EventManager.h"
 
 namespace DUOLGameEngine
 {
 	class NodeBase
 	{
+	public:
+		using PreEvent = EventManager<NodeState, float, NodeBase*, NodeState, NodeState>;
+		using PostEvent = EventManager<NodeState, float, NodeBase*, NodeState, NodeState>;
+		using ChangeEvent = EventManager<NodeState, float, NodeBase*, NodeState, NodeState>;
+
+	private:
+		template <typename T>
+		struct EventInfo
+		{
+			unsigned int _eventID;
+			std::shader_ptr<T> _event;
+		};
+
 		const unsigned int _UID;
 
 		std::string _name;
@@ -15,26 +31,59 @@ namespace DUOLGameEngine
 
 		NodeState _state;
 
+		// Tick 실행전 동작하는 이벤트
+		PreEvent _preEventManager;
+
+		std::vector<EventInfo<PreEvent::EventType>> _preEventList;
+
+		// Tick 실행후 동작하는 이벤트
+		PostEvent _postEventManager;
+
+		std::vector<EventInfo<PostEvent::EventType>> _postEventList;
+
+		// State가 변경될 때 동작하는 이벤트
+		ChangeEvent _changeEventManager;
+
+		std::vector<EventInfo<ChangeEvent::EventType>> _changeEventList;
+
 	public:
 		NodeBase(const std::string& name, NodeType type);
 		
 		~NodeBase() = default;
 
-		const unsigned int GetUID() const;
+		const unsigned int GetUID() const { return _UID; }
 
-		const std::string& GetName() const;
+		const std::string& GetName() const { return _name; }
 
-		NodeType GetType() const;
+		NodeType GetType() const { return _type; }
 
-		NodeState GetState() const;
+		NodeState GetState() const { return _state; }
 
 		void SetState(NodeState state);
+
+		// Idle 상태일 때
+		bool IsHalted() const;
 
 		// Running 상태일 때
 		bool IsRunning() const;
 
 		// Success 혹은 Failure 상태일 때
 		bool IsComplete() const;
+
+		// Tick 실행전 이벤트 추가 / 제거
+		unsigned int AddPreEvent(PreEvent::EventType preEvent);
+
+		bool SubPreEvent(unsigned int preEventID);
+
+		// Tick 실행후 이벤트 추가 / 제거
+		unsigned int AddPostEvent(PostEvent::EventType postEvent);
+
+		bool SubPostEvent(unsigned int postEventID);
+
+		// State 변경시 이벤트 추가 / 제거
+		unsigned int AddChangeEvent(ChangeEvent::EventType changeEvent);
+
+		bool SubChangeEvent(unsigned int changeEventID);
 
 	protected:
 		virtual void Tick() abstract;
