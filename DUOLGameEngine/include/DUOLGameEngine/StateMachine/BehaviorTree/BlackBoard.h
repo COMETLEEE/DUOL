14ad4ebb53f277dc8a97932ded8d4f8f092d88da
CrustPizza <53199927+CrustPizza@users.noreply.github.com>
2022-10-17 11:40:41 +0900

    BlackBoard의 소멸자 관련 구현 추가

--- DUOLGameEngine/include/DUOLGameEngine/StateMachine/BehaviorTree/BlackBoard.h
+++ DUOLGameEngine/include/DUOLGameEngine/StateMachine/BehaviorTree/BlackBoard.h
@@ -1,91 +1,139 @@
 /**
 
-    @file    BlackBoard.h
-    @brief   Behavior Tree에 사용되는 Data들을 기록하는 클래스
-    @details -
-    @author  JKim
-    @date    17.10.2022
+	@file    BlackBoard.h
+	@brief   Behavior Tree에 사용되는 Data들을 기록하는 클래스
+	@details -
+	@author  JKim
+	@date    17.10.2022
 
 **/
 #pragma once
-#include <string>
 #include <map>
+#include <string>
+#include <functional>
 
 namespace DUOLGameEngine
 {
- /**
+	/**
 
-     @class   BlackBoard
-     @brief   Behavior Tree에 사용되는 Data들을 기록하는 클래스
-     @details -
+		@class   BlackBoard
+		@brief   Behavior Tree에 사용되는 Data들을 기록하는 클래스
+		@details -
 
- **/
+	**/
 	class BlackBoard
 	{
-        using Data = void;
-        using TypeKey = unsigned int;
-        using DataStructure = std::map<std::string, Data*>;
+		struct Data
+		{
+			void* _data;
+			std::function<void(void*)> _destructor;
+		};
 
-        std::map<TypeKey, DataStructure> _dataTable;
+		using TypeKey = unsigned int;
+		using DataStructure = std::map<std::string, Data>;
 
-    public:
+		std::map<TypeKey, DataStructure> _dataTable;
 
-        BlackBoard() = default;
+	public:
+		/**
+			@brief   BlackBoard 클래스 default 생성자
+			@details -
+		**/
+		BlackBoard() = default;
 
+		/**
+			@brief   BlackBoard 클래스 소멸자
+			@details Data Type의 소멸자를 호출하여 DataTable 정리
+		**/
+		~BlackBoard();
 
-        ~BlackBoard() = default;
+		/**
+			@brief	 DataTable에 Data 저장
+			@details -
+			@tparam  T       - 저장할 DataType
+			@param   data    - 저장할 Data
+			@param   keyName - 저장할 Data의 Key 값
+		**/
+		template<typename T>
+		void Push(T data, const std::string& keyName);
 
-        template<typename T>
-        void Push(T data, const std::string& keyName);
+		/**
+			@brief	 DataTable로부터 Data 받기
+			@details -
+			@tparam  T       - 가져올 DataType
+			@param   keyName - 가져올 Data의 Key 값
+			@retval  가져올 Data
+		**/
+		template<typename T>
+		T Get(const std::string& keyName);
 
-        template<typename T>
-        const T& Get(const std::string& keyName);
+		/**
+			@brief	 DataTable로부터 Data 꺼내오기
+			@details -
+			@tparam  T       - 가져올 DataType
+			@param   keyName - 가져올 Data의 Key 값
+			@retval  가져올 Data
+		**/
+		template<typename T>
+		T Pop(const std::string& keyName);
 
-        template<typename T>
-        T Pop(const std::string& keyName);
-        
-    private:
-        template<typename T>
-        TypeKey GetTypeKey();
+	private:
+		/**
+			@brief	 DataType에 따른 고유한 Key 값
+			@details -
+			@tparam  T - DataType
+			@retval  고유한 Key 값
+		**/
+		template<typename T>
+		TypeKey GetTypeKey();
 	};
 
-    template<typename T>
-    inline void BlackBoard::Push(T data, const std::string& keyName)
-    {
-        _dataTable[GetTypeKey<T>()][keyName] = static_cast<Data*>(new T(data));
-    }
+	template<typename T>
+	inline void BlackBoard::Push(T data, const std::string& keyName)
+	{
+		static std::function<void(void*)> destructor = [](void* data)
+		{
+			delete static_cast<T*>(data);
+		};
 
-    template<typename T>
-    inline const T& BlackBoard::Get(const std::string& keyName)
-    {
-        auto result = _dataTable[GetTypeKey<T>()].find(keyName);
+		_dataTable[GetTypeKey<T>()][keyName] = { static_cast<void*>(new T(data)), destructor };
+	}
 
-        if (result == _dataTable[GetTypeKey<T>()].end())
-            return T();
+	template<typename T>
+	inline T BlackBoard::Get(const std::string& keyName)
+	{
+		auto result = _dataTable[GetTypeKey<T>()].find(keyName);
 
-        return *static_cast<T*>(result->second);
-    }
+		if (result == _dataTable[GetTypeKey<T>()].end())
+			return T();
 
-    template<typename T>
-    inline T BlackBoard::Pop(const std::string& keyName)
-    {
-        auto result = _dataTable[GetTypeKey<T>()].find(keyName);
+		return *static_cast<T*>(result->second._data);
+	}
 
-        if (result == _dataTable[GetTypeKey<T>()].end())
-            return T();
+	template<typename T>
+	inline T BlackBoard::Pop(const std::string& keyName)
+	{
+		auto result = _dataTable[GetTypeKey<T>()].find(keyName);
 
-        T ret = *static_cast<T*>(result->second);
+		if (result == _dataTable[GetTypeKey<T>()].end())
+			return T();
 
-        _dataTable[GetTypeKey<T>()].erase(result);
+		Data data = result->second;
 
-        return ret;
-    }
+		_dataTable[GetTypeKey<T>()].erase(result);
 
-    template<typename T>
-    inline BlackBoard::TypeKey BlackBoard::GetTypeKey()
-    {
-        static T* typePtr;
+		T ret = *static_cast<T*>(data._data);
 
-        return (TypeKey)&typePtr;
-    }
+		data._destructor(data._data);
+
+		return ret;
+	}
+
+	template<typename T>
+	inline BlackBoard::TypeKey BlackBoard::GetTypeKey()
+	{
+		static T* typePtr;
+
+		return (TypeKey)&typePtr;
+	}
 }
--- DUOLGameEngine/src/DUOLGameEngine/StateMachine/BehaviorTree/BlackBoard.cpp
+++ DUOLGameEngine/src/DUOLGameEngine/StateMachine/BehaviorTree/BlackBoard.cpp
@@ -2,5 +2,14 @@
 
 namespace DUOLGameEngine
 {
+	BlackBoard::~BlackBoard()
+	{
+		for (auto& iter : _dataTable)
+		{
+			DataStructure& structure = iter.second;
 
+			for (auto iter = structure.begin(); iter != structure.end(); iter++)
+				iter->second._destructor(iter->second._data);
+		}
+	}
 }
